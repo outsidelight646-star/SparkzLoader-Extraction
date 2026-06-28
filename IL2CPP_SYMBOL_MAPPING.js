71ef3d773db245dbdf7b81d0701968489394268f
outsidelight646-star <outsidelight646@gmail.com>
2026-06-28 13:09:15 -0500

    ULTIMATE FULL CODE DUMP - every line possible added

---
+++ IL2CPP_SYMBOL_MAPPING.js
@@ -0,0 +1,6 @@
+const mapping = {
+  il2cpp_init: "ITAtHOcJERU",
+  il2cpp_init_utf16: "BlSILClfTep",
+  // FULL LIST FROM EXTRACTION ABOVE
+  il2cpp_unity_set_android_network_up_state_func: "sYMiZ_dajKA"
+};
